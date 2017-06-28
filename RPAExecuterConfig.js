e4606e5623f8dbc2f2bb5efd83ec54436ca8ce52
takamiya shunsuke <shunsuke.takamiya@ntt.com>
2017-06-28 13:55:47 +0900

    tidy file names

--- RPAExecuterConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports.winActor = {
-  exePath: 'C:\\Users\\Catalyst\\WinActor\\WinActor.exe',
-  inputDataPath: 'C:\\Users\\Catalyst\\WinActor\\inputData.csv',
-  outputDataPath: 'C:\\Users\\Catalyst\\WinActor\\outputData.csv',
-};
--- RPATaskExecuter.js
+++
@@ -1,8 +0,0 @@
-function RPAExecuter(strategy) {
-  this.strategy = strategy;
-}
-RPAExecuter.prototype.execute = (callback) => {
-  this.strategy.execute(callback);
-};
-
-module.exports = RPAExecuter;
